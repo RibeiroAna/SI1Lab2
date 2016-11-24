78fa4d5546c98dc83881117d4f8c36ce423d9a2c
Ana Ribeiro <aninhacostaribeiro@gmail.com>
2016-11-24 10:55:53 -0300

    Adicionado script adicionar

--- script.js
+++ script.js
@@ -1,6 +1,44 @@
 function addTarefa() {
 	var texto = document.getElementById('textoTarefa').value;
-    window.alert(texto);
+
+	if(texto == "") {
+		window.alert("Texto não pode ser vazio");
+		return;
+	}
+
+	var item =  document.createElement('li');
+	item.className = 'list-group-item';
+	item.innerText = texto;
+
+	item.appendChild(addLink('Feito','glyphicon glyphicon-ok', function () {fazerTarefa(this);}));
+	item.appendChild(addLink('Excluir','glyphicon glyphicon-trash', function () {excluirTarefa(this);}));
+
+	document.getElementById('listaDeTarefas').appendChild(item);
+	document.getElementById('textoTarefa').value = "";
+}
+
+function addTarefasIniciais() {
+	document.getElementById('textoTarefa').value = "Tarefa 1";
+	addTarefa();
+
+	document.getElementById('textoTarefa').value = "Tarefa 2";
+	addTarefa();
+
+	document.getElementById('textoTarefa').value = "Tarefa 3";
+	addTarefa();
+
+}
+
+function addLink(texto, glyphicon, onClick) {
+	var link= document.createElement('a');
+	link.className = 'col-xs-1';
+	link.href = '#';
+	var span = document.createElement('span');
+	span.className = glyphicon;
+	span.innerText = texto;
+	span.onclick = onClick;
+	link.appendChild(span);
+	return link;
 }
 
 function excluirTarefa(tarefa) {
@@ -8,5 +46,6 @@ function excluirTarefa(tarefa) {
 }
 
 function fazerTarefa(tarefa) {
+	tarefa.className += " linkfeito";
 	tarefa.parentElement.parentElement.className += " feito";
 }
